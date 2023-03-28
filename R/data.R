72d001d7277e37f3fabe1c3edace16c9da9abc26
Marco Rheinnecker <marco.rheinnecker@gmail.com>
2023-03-28 16:22:08 +0200

    added usage documentation to data

--- R/data.R
+++ R/data.R
@@ -1,20 +1,20 @@
 #' copynumber object
-#'
+#' @usage data(GR_SCNA)
 #' @format ## `GR_SCNA`
 #' GRanges object
 "GR_SCNA"
 #' somatic small variant object
-#'
+#' @usage data(GR_GERM_SMALL_VARS)
 #' @format ## `GR_GERM_SMALL_VARS`
 #' GRanges object
 "GR_SOM_SMALL_VARS"
 #' germline small variant object
-#'
+#' @usage data(GR_SOM_SMALL_VARS)
 #' @format ## `GR_SOM_SMALL_VARS`
 #' GRanges object
 "GR_GERM_SMALL_VARS"
 #' germline small variant object
-#'
+#' @usage data(GR_GENE_MODEL)
 #' @format ## `GR_GENE_MODEL`
 #' GRanges object
 "GR_GENE_MODEL"
